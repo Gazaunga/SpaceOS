0c2c898807e82b1d8280ca6b4300a949bfd86fe3
Gazaunga <Gazaunga@gmail.com>
2017-08-08 12:37:39 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,11 @@ git config --global user.email "jeremy.ottley@gmail.com"
 git config --global core.autocrlf input
 git config --global core.safecrlf true
 cd ~
+wget https://raw.githubusercontent.com/Gazaunga/ui3/master/.zshrc
+mkdir zsh
+cd zsh
+wget https://raw.githubusercontent.com/Gazaunga/ui3/master/zsh/minimal.zsh
+cd ~
 wget -O ruby-install-0.6.1.tar.gz https://github.com/postmodern/ruby-install/archive/v0.6.1.tar.gz
 tar -xzvf ruby-install-0.6.1.tar.gz
 cd ruby-install-0.6.1/
